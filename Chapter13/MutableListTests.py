6c857ed3ade612dff25f0847b189398375483fee
Th2mas <tkhlebovitch@hotmail.com>
2023-09-05 20:44:09 +0200

    Show how to use append, extend, insert, pop and remove on a list

--- Chapter13/MutableListTests.py
+++ Chapter13/MutableListTests.py
@@ -43,3 +43,98 @@ class MutableListTests(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             l[0:9:2] = ['A', 'B']
+
+    def test_del_operator_remove_one_element(self):
+        numbers = [1, 2, 3, 4]
+        self.assertEqual(numbers[1], 2)
+        self.assertEqual(len(numbers), 4)
+
+        del numbers[1]
+        self.assertEqual(numbers[1], 3)
+        self.assertEqual(len(numbers), 3)
+
+    def test_del_sublist(self):
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        del numbers[1:3]
+        self.assertEqual(numbers, [1, 4, 5, 6, 7, 8, 9])
+
+    def test_del_sublist_with_slicing(self):
+        numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        del numbers[::2]
+        self.assertEqual(numbers, [1, 3, 5, 7, 9])
+
+    def test_append(self):
+        elements = ['Water', 'Earth', 'Fire']
+
+        elements.append('Air')
+        self.assertEqual(elements, ['Water', 'Earth', 'Fire', 'Air'])
+
+    def test_extend(self):
+        elements = ['Water', 'Earth', 'Fire', 'Air']
+        new_elements = ['Metal', 'Lightning', 'Sand']
+
+        elements.extend(new_elements)
+        self.assertEqual(elements, ['Water', 'Earth', 'Fire', 'Air', 'Metal', 'Lightning', 'Sand'])
+
+    def test_insert_regular_case(self):
+        numbers = [1, 2, 3, 4, 5, 7, 8, 9]
+        numbers.insert(0, 0)
+        numbers.insert(6, 6)
+
+        self.assertEqual(numbers, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_insert_index_is_too_small(self):
+        nums = [0, 1, 2]
+        nums.insert(-15, -1)
+
+        self.assertEqual(nums, [-1, 0, 1, 2])
+
+    def test_insert_index_is_too_big(self):
+        nums = [0, 1, 2]
+        nums.insert(83, 3)
+
+        self.assertEqual(nums, [0, 1, 2, 3])
+
+    def test_pop_without_argument(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        text.pop()
+        self.assertEqual(text, ['H', 'e', 'l', 'l'])
+
+    def test_pop_with_argument(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        text.pop(1)
+        self.assertEqual(text, ['H', 'l', 'l', 'o'])
+
+    def test_pop_with_negative_index_removes_element_from_the_back(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        text.pop(-1)
+        self.assertEqual(text, ['H', 'e', 'l', 'l'])
+
+    def test_pop_with_too_small_index_raises_index_error(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        with self.assertRaises(IndexError):
+            text.pop(-6)
+
+    def test_pop_with_too_large_index_raises_index_error(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        with self.assertRaises(IndexError):
+            text.pop(99)
+
+    def test_remove_removes_first_occurrence_of_passed_argument(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        text.remove('e')
+        self.assertEqual(text, ['H', 'l', 'l', 'o'])
+
+    def test_remove_raises_value_error_if_passed_argument_does_not_exist_in_list(self):
+        text = ['H', 'e', 'l', 'l', 'o']
+
+        with self.assertRaises(ValueError):
+            text.remove('z')
